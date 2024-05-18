1dab6b9b685c991b209de05a706080331aa6c7cf
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2024-05-18 18:29:20 +0200

    Create app

---
+++ src/main/java/pl/com/itsystems/homeBudget/HomeBudgetApplication.java
@@ -0,0 +1,13 @@
+package pl.com.itsystems.homeBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBudgetApplication.class, args);
+    }
+
+}
